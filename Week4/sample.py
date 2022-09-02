cfc1a7bdac139f0b5a277a8bb1b49d8a7b7655c7
thanh quach <thanhb.quach@gmail.com>
2022-09-02 12:06:34 -0400

    programming with python completed

--- Week4/sample.py
+++ Week4/sample.py
@@ -1,3 +1,21 @@
 import os
 str = os.listdir(r'C:\Users\thanh quach\projects\python\Week4')
 print(str)
+
+sentence = 'I am the best'
+
+def word_count(a):
+    words =len(a.split())
+    print(words)
+    return
+
+print(sentence[0].isupper())
+
+from math import pi 
+print(pi)
+
+for x in range(1, 4):
+    print(int((str((float(x))))))
+
+
+
