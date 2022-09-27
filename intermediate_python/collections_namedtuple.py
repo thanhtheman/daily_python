47a1235aa057e09f41831f7c333d7191c7ac9799
thanh quach <thanhb.quach@gmail.com>
2022-09-26 21:06:26 -0400

    collections deque

--- intermediate_python/collections_namedtuple.py
+++ intermediate_python/collections_namedtuple.py
@@ -1,16 +1,29 @@
-from collections import namedtuple, OrderedDict, defaultdict
+from collections import namedtuple, OrderedDict, defaultdict, deque
 
 Point = namedtuple('Point', 'x,y')
 pt = Point(1,5)
-print(pt)
-print(pt.x, pt.y)
+# print(pt)
+# print(pt.x, pt.y)
 
-dictionary = OrderedDict()
+# dictionary = OrderedDict()
+dictionary = defaultdict(int)
 dictionary['a'] = 8
 dictionary['b'] = 15
 dictionary['c'] = 12
 dictionary['d'] = 85
 dictionary['e'] = 18
-print(dictionary)
-print(dictionary.keys())
+# print(dictionary)
+# print(dictionary.keys())
 
+d = deque()
+
+d.append(1)
+d.append(2)
+d.appendleft(4)
+
+print(d)
+print(list(d)[0])
+
+d.popleft()
+
+print(d)
